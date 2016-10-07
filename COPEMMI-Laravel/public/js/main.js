bcd19d10366b8c6a8c131f14d0bdf14abfd29883
cardomaggot <cardovargas@hotmail.com>
2016-10-07 01:08:28 -0600

    parte 1

---
+++ COPEMMI-Laravel/public/js/main.js
@@ -0,0 +1,53 @@
+/*Carga el documento jquery, carga todo el documento jquery*/
+/*Función para abrir y ocultar los submenús*/ 
+
+$(document).ready(function(){
+	$('.menu li:has(ul)').click(function(e){ //accede los elementos del submenú. 
+		e.preventDefault();
+
+		if ($(this).hasClass('activado')){
+			$(this).removeClass('activado'); 
+			$(this).children('ul').slideUp(); 
+		}
+		else {
+			$('.menu li ul').slideUp(); //Para ocultar submenus. 
+			$('.menu li').removeClass('activado');
+			$(this).addClass('activado');
+			$(this).children('ul').slideDown(); 
+		}
+	}); 
+
+/*Abrir y cerrar el menú vertical*/
+
+var contador = 1;
+
+$('.bt-menu').click(function(){
+
+if(contador == 1){
+
+	$('.sidebar').animate({
+		left:'0'
+	});
+	contador=0;
+		} else {
+			contador = 1;
+			$('.sidebar').animate({
+				left:'-100%'
+			})
+		}
+});
+
+$('.menu li ul li a').click(function(){
+
+	window.location.href = $(this).attr("href");
+
+});
+
+});
+/*
+$('.menu li ul li a').click(funtion(){
+
+	window.location.href = $(this).attr("href");
+
+});
+*/
