323ddf3534f24386aeedb5c7ac72c75801ff9696
Luissan0717 <luisktkd@hotmail.com>
2016-10-19 13:06:33 -0600

    Se le agrega una línea para que no muestre todo

--- COPEMMI-Laravel/app/Http/Controllers/MaterialesController.php
+++ COPEMMI-Laravel/app/Http/Controllers/MaterialesController.php
@@ -46,7 +46,8 @@ class MaterialesController extends Controller
               break; 
 
           default:      
-           $materiales=material::buscadorNombre($request->buscar)->orderBy('COD_MATERIAL','DESC')->paginate(100);//si se le quita el paginate, no mostrara nada
+           //$materiales=material::buscadorNombre($request->buscar)->orderBy('COD_MATERIAL','DESC')->paginate(100);//si se le quita el paginate, no mostrara nada
+            $materiales=material::buscadorNombre(" ")->paginate(1);
       }
 
         return View('Sistema')->with('materiales',$materiales);
