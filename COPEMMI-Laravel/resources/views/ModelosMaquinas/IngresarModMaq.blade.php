e97d7b5efb3b48c6fd0fd50ead6288358fa737ac
Luissan0717 <luisktkd@hotmail.com>
2016-10-24 20:53:25 -0600

    Tamaño de botón, un nuevo js

--- COPEMMI-Laravel/resources/views/ModelosMaquinas/IngresarModMaq.blade.php
+++ COPEMMI-Laravel/resources/views/ModelosMaquinas/IngresarModMaq.blade.php
@@ -49,7 +49,7 @@
 
 							
 				<div class="col-md-6">
-				<input type="file" id="imagen"'>
+				<input type="file" id="imagen" size="15"'>
 				<p class="help-block">Máximo 50mb </p>
 				</div>
 						</div>
@@ -127,5 +127,5 @@
 
 
 @section('js')
-	<!--incluir el js para validar-->
+	{!! Html::script('js/validacionMod.js') !!}
 @stop
