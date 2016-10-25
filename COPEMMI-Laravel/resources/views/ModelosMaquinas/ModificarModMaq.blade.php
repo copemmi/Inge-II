2c448b2ef148930ad62c8f1f38936203c69aac7e
copemmi <marialaura.alfaro@outlook.com>
2016-10-24 22:42:18 -0600

    Vista encabezado modelos de máquinas.

--- COPEMMI-Laravel/resources/views/ModelosMaquinas/ModificarModMaq.blade.php
+++ COPEMMI-Laravel/resources/views/ModelosMaquinas/ModificarModMaq.blade.php
@@ -3,8 +3,36 @@
 
 <!--------------------------------------------------------------------TABLA EN EL CENTRO------------------------------------------------------>
 
-		<h1>Modificar Modelos Máquinas</h1>
+		<div id="center">
+			
+			<div class="content">
+				
+				<div class="main-image">
+					<div id="page-header">		
+			  			<a class="text-left"><img src="{{ asset('imagenes/tek.PNG')}}";/></a>
+					</div>
+				</div>
 
+				<div class="page-header">
+	  				<h1 class="text-center">Modificar Modelos Máquinas</h1>
+				</div>
+
+				<!-- MENSAJE DE ERROR, SI UN DATO ES ERRONEO -->
+					@if (count($errors) > 0)
+	    				<div class="alert alert-danger">
+		       				 <ul>
+					            @foreach ($errors->all() as $error)
+					                <li>{{ $error }}</li>
+					            @endforeach
+		        			</ul>
+	    				</div>
+					@endif
+				<!-- FIN DE MSJ DE ERROR -->
+
+
+			</div>
+
+		</div>
 
 @stop
 
