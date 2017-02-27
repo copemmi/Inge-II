e6ad94151a560ac93dbab6a53f0fe4673639205f
Johel <johelopezcr@hotmail.com>
2017-02-26 19:15:44 -0600

    modificacion en el js de insertar orden

--- COPEMMI-Laravel/public/js/validarOrdFab.js
+++ COPEMMI-Laravel/public/js/validarOrdFab.js
@@ -4,11 +4,10 @@ function inicio(){
 validarCedula();//se puso por si hay un error en otro campo y si la cedula esta bien que se habilite el boton, sin necesidad de apretar una tecla en el text de cedula
 $("span.help-block").hide();
 $("#COD_ORDEN_FABRICACION").keyup(validarCodigo);
-$("#NOMBRE_CLIENTE").keyup(validarNomb);
-$("#CEDULA_CLIENTE").keyup(validarCedula);
+
 }
 
-$('#Guardar').hide();
+//$('#Guardar').hide();
 
 function validarCodigo(){
 	var valor = document.getElementById("COD_ORDEN_FABRICACION").value;
@@ -37,73 +36,4 @@ function validarCodigo(){
 	}
 }
 
-function validarNomb(){
-	var valor = document.getElementById("NOMBRE_CLIENTE").value;
-	if(valor == null || valor.length == 0 || /^\s+$/.test(valor)){
-
-		$("#icononom").remove();
-		$("#NOMBRE_CLIENTE").parent().parent().attr("class","form-group has-error has-feedback")
-		$("#NOMBRE_CLIENTE").parent().children("span").text("Debe ingresar algun caracter").show();
-		$("#NOMBRE_CLIENTE").parent().append("<span id='icononom' class='glyphicon glyphicon-remove form-control-feedback'></span>");
-		return false;
-	}
-	else if(valor.length > 50){
-
-		$("#icononom").remove();
-		$("#NOMBRE_CLIENTE").parent().parent().attr("class","form-group has-error has-feedback")
-		$("#NOMBRE_CLIENTE").parent().children("span").text("Debe ser menor que 50 caracteres").show();
-		$("#NOMBRE_CLIENTE").parent().append("<span id='icononom' class='glyphicon glyphicon-remove form-control-feedback'></span>");
-		return false;
-	}
-	else{
-		$("#icononom").remove();
-		$("#NOMBRE_CLIENTE").parent().parent().attr("class","form-group has-success has-feedback")
-	    $("#NOMBRE_CLIENTE").parent().children("span").text("").hide();
-	    $("#NOMBRE_CLIENTE").parent().append("<span id='icononom' class='glyphicon glyphicon-ok form-control-feedback'></span>");
-	    return true;
-	}
-}
-
-
-function validarCedula(){
-
-	//var tester= /^[0-9]$/;
-	var tester = /\d{1}-0\d{3}-0\d{3}/
-
-	var valor = document.getElementById("CEDULA_CLIENTE").value;
-	if(valor == null || valor.length == 0 || /^\s+$/.test(valor)){
-
-		$("#iconoce").remove();
-		$("#CEDULA_CLIENTE").parent().parent().attr("class","form-group has-error has-feedback")
-		$("#CEDULA_CLIENTE").parent().children("span").text("Debe ingresar algun caracter").show();
-		$("#CEDULA_CLIENTE").parent().append("<span id='iconoce' class='glyphicon glyphicon-remove form-control-feedback'></span>");
-		$('#Guardar').hide();
-		return false;
-	}
-	else if(valor.length > 11){
-
-		$("#iconoce").remove();
-		$("#CEDULA_CLIENTE").parent().parent().attr("class","form-group has-error has-feedback")
-		$("#CEDULA_CLIENTE").parent().children("span").text("Debe ser menor que 11 caracteres").show();
-		$("#CEDULA_CLIENTE").parent().append("<span id='iconoce' class='glyphicon glyphicon-remove form-control-feedback'></span>");
-		$('#Guardar').hide();
-		return false;
-	}
-	else if(tester.test(valor)==false){
-		$("#iconoce").remove();
-		$("#CEDULA_CLIENTE").parent().parent().attr("class","form-group has-error has-feedback")
-		$("#CEDULA_CLIENTE").parent().children("span").text("Debe tener formato #-0###-0###").show();
-		$("#CEDULA_CLIENTE").parent().append("<span id='iconoce' class='glyphicon glyphicon-remove form-control-feedback'></span>");
-		$('#Guardar').hide();
-		return false;
-	}
-	else{
-		$("#iconoce").remove();
-		$("#CEDULA_CLIENTE").parent().parent().attr("class","form-group has-success has-feedback")
-	    $("#CEDULA_CLIENTE").parent().children("span").text("").hide();
-	    $("#CEDULA_CLIENTE").parent().append("<span id='iconoce' class='glyphicon glyphicon-ok form-control-feedback'></span>");
-	    $('#Guardar').show();
-	    return true;
-	}
-}
 
