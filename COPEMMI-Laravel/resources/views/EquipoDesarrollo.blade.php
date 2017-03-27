2bcc38c23d5ba2e0a9668409ce0dd8d4ead6fd7c
Sanchez7 <luisktkd@hotmail.com>
2017-03-27 16:01:51 -0600

    :v x3

--- COPEMMI-Laravel/resources/views/EquipoDesarrollo.blade.php
+++ COPEMMI-Laravel/resources/views/EquipoDesarrollo.blade.php
@@ -12,53 +12,54 @@
 			</div>
 
 			<div class="page-header">
-				<h1 class="text-center">Equipo de Desarrollo</h1>
+				<h1 class="text-center">Equipo Desarrollador</h1>
 			</div>
 
 			<div class="container">
 				
 				<div class="form-group">
+                    <div class="col-md-8">
+                        <h4>Luis Carlos Sánchez Azofeifa<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
+                        <h4>Ingeniero en Sistemas<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-8">
+                        <h4>María Laura Alfaro Espinoza<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
+                        <h4>Ingeniera en Sistemas<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-8"> 
+                        <h4>Ricardo Vargas Hernández<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
+                        <h4>Ingeniero en Sistemas<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
+                    </div>
+                </div>
+
+                <div class="form-group">
 					<div class="col-md-8">
-						<h4>Josué Hidalgo.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4> 
-						<h4>Ingeniero en Sistemas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
+						<h4>Josué Hidalgo Ramirez<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4> 
+						<h4>Ingeniero en Sistemas<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
                 	</div>
 				</div>
 
-				<div class="form-group">
-					<div class="col-md-8"> 
-						<h4>Ricardo Vargas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
-						<h4>Ingeniero en Sistemas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
+                <div class="form-group">
+                    <div class="col-md-8">
+                        <h4>Johel López Alpizar<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
+                        <h4>Ingeniero en Sistemas<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="col-md-8">
-                        <h4>Luis Carlos Sánchez.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
-                        <h4>Ingeniero en Sistemas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
+                        <h4>Melvin Mora Sibaja<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
+                        <h4>Ingeniero en Sistemas<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
                     </div>
                 </div>
 
-                <div class="form-group">
-                    <div class="col-md-8">
-                        <h4>Johel López.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
-                        <h4>Ingeniero en Sistemas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-md-8">
-                        <h4>Melvin Mora.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
-                        <h4>Ingeniero en Sistemas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-md-8">
-                        <h4>María Laura Alfaro Espinoza.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span></h4>
-                        <h4>Ingeniera en Sistemas.<span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-briefcase"></i></span></h4>
-                    </div>
-
-                </div> 
+                
 			</div>
 		</div>
 
